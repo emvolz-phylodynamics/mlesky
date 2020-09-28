0b88430f5a87b213d7cc867bb5c5f02de241eadb
xavierdidelot <xavier.didelot@gmail.com>
2020-09-28 09:26:57 +0100

    Resolve warnings

---
+++ R/package.R
@@ -0,0 +1,14 @@
+#' @name mlesky
+#' @title BactDating: Maximum likelihood estimation of effective population size through time, growth rates, and phylodynamic regressions
+#'
+#' @description BactDating is a R package to perform Maximum likelihood estimation of effective population size through time, growth rates, and phylodynamic regressions.
+#'
+#' @author Erik Volz \email{erik.volz@gmail.com}
+#'
+#' @seealso https://github.com/emvolz-phylodynamics/mlesky
+#'
+#' @import stats
+#' @import graphics
+#' @import ape
+#' @import utils
+NULL
